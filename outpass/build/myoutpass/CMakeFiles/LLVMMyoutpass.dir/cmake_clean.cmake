e379a4710333bf6b15e981ab277e13ca5f922f11
chan233 <chan233@chan.com>
2023-08-20 08:41:35 +0800

    init

---
+++ outpass/build/myoutpass/CMakeFiles/LLVMMyoutpass.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LLVMMyoutpass.dir/outpass.cpp.o"
+  "CMakeFiles/LLVMMyoutpass.dir/outpass.cpp.o.d"
+  "libLLVMMyoutpass.pdb"
+  "libLLVMMyoutpass.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LLVMMyoutpass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ outpass/build/myoutpass/CMakeFiles/LLVMMyoutpass.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for LLVMMyoutpass.
+# This may be replaced when dependencies are built.
